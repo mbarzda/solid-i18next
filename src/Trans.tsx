d9f8c19e949ed18ad5e2ac5a7bb1bfba088094c8
Martynas Barzda <martynas@barzda.pro>
2022-10-12 16:08:56 +0000

    Update dependencies, cleanup types

--- src/Trans.tsx
+++ src/Trans.tsx
@@ -1,8 +1,8 @@
 import { TFunctionKeys, TOptions } from 'i18next';
-import { Component } from 'solid-js';
+import { FlowComponent } from 'solid-js';
 import { useTransContext } from './TransProvider';
 
-export const Trans: Component<{ children?: string; key: TFunctionKeys; options?: TOptions | string }> = (props) => {
+export const Trans: FlowComponent<{ key: TFunctionKeys; options?: TOptions | string }, string> = (props) => {
   const [t] = useTransContext();
   return <>{t(props.key, props.children, props.options)}</>;
 };
