500f6575975c377df483112c7dd0db9bf460b897
Supertiger <saawalkhn@googlemail.com>
2022-12-28 23:39:36 +0000

    Allow components inside Trans component

--- src/Trans.tsx
+++ src/Trans.tsx
@@ -1,8 +1,32 @@
 import { TFunctionKeys, TOptions } from 'i18next';
-import { FlowComponent } from 'solid-js';
+import { JSXElement } from 'solid-js';
 import { useTransContext } from './TransProvider';
+import HTML from 'html-parse-stringify';
 
-export const Trans: FlowComponent<{ key: TFunctionKeys; options?: TOptions | string }, string> = (props) => {
+type FunctionalJSXElement = ((data: Record<string, string>) => JSXElement);
+interface TransProps { 
+  key: TFunctionKeys; 
+  options?: TOptions | string;
+  children: JSXElement | FunctionalJSXElement; 
+}
+
+export const Trans = (props: TransProps) => {
   const [t] = useTransContext();
-  return <>{t(props.key, props.children, props.options)}</>;
+
+  const result = () => {
+    const translation = t(props.key, props.options);
+    const ast = HTML.parse(`<0>${translation}</0>`)[0].children;
+    const data = {};
+    ast.forEach(component => {
+      if (component.type !== "tag") return;
+      data[component.name] = component.children[0].content;
+    });
+    return (props.children as FunctionalJSXElement)(data);
+  }
+
+  return (
+    <>{
+      typeof props.children === "string" ? t(props.key, props.children, props.options) : result() 
+    }</>
+  );
 };
